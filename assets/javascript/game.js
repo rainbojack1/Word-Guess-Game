697e69a6d2fac43882dd6b276386d48c660bbdf5
Rainbo <jesselyn.jackson@gmail.com>
2019-06-17 08:28:47 -0400

    Started building out some functions to make the game work: reset, alphaOnly, play, and generateWord.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
 var characters = ["Mickey", "Aladdin", "Jasmin", "Jafar", "Genie", "Simba", "Nala", "Timon", "Pumba", "Scar"];
 var winCount = 0;
 var win = document.getElementById("wins");
+var guess = document.getElementById("guessesLeft");
+var answerList = [];
+var answered = document.getElementById("lettersGuessed");
+var hidden = document.getElementById("hidden");
+var i = 20;
 
 win.textContent = winCount;
 
@@ -11,4 +16,45 @@ function generateWord() {
     return word;
 }
 
-generateWord();
+function play() {
+    //Get a random letter
+    generateWord();
+    if(i > 0){
+        var hiddenWord = [];
+        var strignToDisplay = "";
+        for(var j = 0; j < word.length; j++){
+           hiddenWord.push("_"); 
+           strignToDisplay+= hiddenWord[j] + "       ";
+           hidden.textContent = strignToDisplay;
+        }
+         
+        document.onkeyup = function(event) {
+            alphaOnly();
+            //if key press matches one of the letters of the generated word, display that word in lieu of a _
+        }
+
+        win.textContent = winCount;
+        guess.textContent = i;
+        answered.textContent = answerList;
+      
+    }
+}
+
+function alphaOnly(event) {
+    var key = event.keyCode;
+    return (key >= 65 && key <= 90);
+  };
+
+function reset(){
+    if(i === 0){
+        alert("Game Over!");
+        winCount = 0;
+        loseCount = 0;
+        i = 10;
+        answerList = [];
+    }
+}
+
+
+//generateWord();
+play();
